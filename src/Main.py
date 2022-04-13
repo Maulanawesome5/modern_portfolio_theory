b26341547918d4b8c97d9dcf450e2f3e53dd4fb3
Maulanawesome5 <maulanawesome5@gmail.com>
2022-04-14 04:55:38 +0700

    Mengubah seluruh isi Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,6 @@
-# dummy program untuk membuat modern portfolio theory
-from tkinter import *
-from tkinter import ttk
-from pandas_datareader import DataReader
+from tkinter import BOTH, CENTER, LEFT, TOP, ttk
 from pymysql import *
+from pandas_datareader import DataReader
 import tkinter
 
 """
@@ -31,125 +29,84 @@ id | kode_sektor |      sektor_usaha        | kodeSaham |         namaEmiten    
 80 |IDX:H        |Properti & Real Estate    |URBN       |Urban Jakarta Propertindo Tbk | 2019 Nov 10 | 1234567890 | Pengembangan |
 """
 
-# top level widget
-root = tkinter.Tk()
-root.title("Optimasi Portofolio Saham")
-root.iconbitmap("D:/Aji/[ICON FILE]/business-color_stock_icon-icons.com_53431.ico")
+### MySQL Connection
+Koneksi = Connection(host="localhost", user="root", database="saham_indonesia")
+Kursor = Koneksi.cursor()
 
+## Mengambil data daftar sektor bursa efek indonesia
+Kursor.execute("""SELECT * FROM daftar_sektor;""")
+Daftar_Sektor = Kursor.fetchall()
+# print(Daftar_Sektor, "\n")
 
-# top level frame
-root_frame = Frame(
-    master=root,
-    background="#535353",
-    padx=10,
-    pady=10
-)
-root_frame.grid(column=0, row=0, sticky=(N, W, E, S), columnspan=10, rowspan=10)
+# variabel list kosong
+Kode_Sektor = []
+Nama_Sektor = []
+for i in range(len(Daftar_Sektor)):
+    Kode_Sektor.append(Daftar_Sektor[i][0])
+    Nama_Sektor.append(Daftar_Sektor[i][1])
 
-# MySQL Parameter
-koneksi = Connection(host='localhost', user='root', database='saham_indonesia')
-kursor = koneksi.cursor()
+# print(Kode_Sektor, "\n")
+# print(Nama_Sektor, "\n")
 
-# method untuk memilih sektor
-def pilihSektor():
-    # second widget untuk memilih jenis sektor
-    sektor_window = tkinter.Toplevel()
-    sektor_window.title('Pilih Sektor Saham')
-    sektor_window.iconbitmap("D:/Aji/[ICON FILE]/business-color_stock_icon-icons.com_53431.ico")
-    # sektor_window.geometry("400x400")
-    
-    sektor_window_frame = Frame(master=sektor_window, background="#535353", padx=10, pady=10)
-    sektor_window_frame.grid(column=0, row=0, sticky=(N, W, E, S), columnspan=10, rowspan=10)
+## Mengambil data daftar saham berdasarkan sektor yang dipilih
+def Select_Stocks():
+    ### MySQL Connection
+    Koneksi = Connection(host="localhost", user="root", database="saham_indonesia")
+    Kursor = Koneksi.cursor()
+    Kursor.execute(f"SELECT * FROM daftar_saham_indonesia WHERE sektor_usaha = '{Input_Sektor.get()}';")
+    Daftar_Saham = Kursor.fetchall()
+    # print(Daftar_Saham, "\n")
 
-    # MySQL Parameter
-    koneksi = Connection(host='localhost', user='root', database='saham_indonesia')
-    kursor = koneksi.cursor()
-    
-    kode_sektor = ['IDX:A', 'IDX:B', 'IDX:C', 'IDX:D', 'IDX:E', 'IDX:F', 'IDX:G', 'IDX:H', 'IDX:I', 'IDX:J', 'IDX:K', 'IDX:Z']
-    nama_sektor = [
-        'Energi',
-        'Barang Baku (coming soon)',
-        'Perindustrian (coming soon)',
-        'Barang Konsumen Primer (coming soon)',
-        'Barang Konsumen Non-Primer (coming soon)',
-        'Kesehatan',
-        'Keuangan (coming soon)',
-        'Properti & Real Estate',
-        'Teknologi',
-        'Infrastruktur',
-        'Transportasi & Logistik',
-        'Produk Investasi Tercatat (coming soon)'
-    ]
+    global Ticker_Saham
+    global Nama_Emiten
 
-    indexNumber = 0
-    valueNumber = 0
-    rowNumber = 0
-    kolomList = 0
+    # variabel list kosong
+    Ticker_Saham = []
+    Nama_Emiten = []
+    for i in range(len(Daftar_Saham)):
+        Ticker_Saham.append(Daftar_Saham[i][3])
+        Nama_Emiten.append(Daftar_Saham[i][4])
 
-    # Loop thru results
-    for i in range(13):
-        kodesektor = kode_sektor[indexNumber]
-        namasektor = nama_sektor[indexNumber]
+    # print(Ticker_Saham, "\n", len(Ticker_Saham))
+    # print(Nama_Emiten, "\n", len(Nama_Emiten))
 
-        # showLabel = Label(master=second_frame, text=valueNumber, background="#5A5A5A", foreground="#FFFFFF")
-        # showLabel.grid(column=0, row=rowNumber, padx=10, pady=5, sticky=W)
+    Koneksi.commit()
+    Koneksi.close()
 
-        kode_sektorLabel = Checkbutton(
-            master=sektor_window_frame,
-            text=kodesektor,
-            background="#535353",
-            activeforeground="#FFFFFF",
-            variable=StringVar(sektor_window_frame, kodesektor)
+    # Second window untuk menampilkan daftar saham
+    Window_Saham = tkinter.Toplevel()
+    Window_Saham.title(f"Daftar Saham Sektor {Input_Sektor.get()}")
+    Window_Saham.iconbitmap('D:/LATIHAN PEMROGRAMAN/(METODE PENELITIAN)/icon/business_stock.ico')
+
+    Frame_Window_Saham = tkinter.Frame(master=Window_Saham, background="#535353", height=300, width=400)
+    Frame_Window_Saham.pack(fill=BOTH, anchor=CENTER, side=TOP)
+
+    Mini_Frame = tkinter.Frame(
+        master=Window_Saham, width=40, height=1, background="#535353", relief=tkinter.GROOVE, border=2,
+    ) # Mini frame untuk mengatur layout 2 button
+    Mini_Frame.pack(fill=BOTH, anchor=CENTER, side=TOP)
+
+    for i in range(len(Daftar_Saham)):
+        Pilihan_Saham = tkinter.Checkbutton(
+            master=Frame_Window_Saham, background="#535353", foreground="#ffffff", padx=10, 
+            selectcolor="#2d2d2d", activebackground="#535353", activeforeground="#ffffff",
+            text=(str(Ticker_Saham[i]) + "\t" + str(Nama_Emiten[i])), textvariable=Ticker_Saham[i]
         )
-        kode_sektorLabel.grid(column=0, row=rowNumber, pady=5, padx=2, sticky=W)
+        Pilihan_Saham.grid(column=0, row=i, sticky=tkinter.W)
 
-        nama_sektorLabel = Label(master=sektor_window_frame, text=namasektor, background="#535353", foreground="#FFFFFF")
-        nama_sektorLabel.grid(column=1, row=rowNumber, pady=5, padx=1, sticky=W)
+    Get_Option_Saham = tkinter.Button(
+        master=Mini_Frame, text="Simpan Pilihan",
+        background="#535353", foreground="#ffffff", activebackground="#707070", activeforeground="#ffffff", 
+        command=Pilihan_Saham.select()
+    )
+    Get_Option_Saham.grid(column=0, row=0, sticky=tkinter.W)
 
-        valueNumber += 1
-        rowNumber += 1
-        indexNumber += 1
-
-    def clicked(value):
-        myLabel = Label(master=sektor_window_frame, text=kodesektor)
-        myLabel.grid(column=1, row=0, sticky=W)
-    
-    
-    saveButton = Button(master=sektor_window_frame, text="Save Option", command=lambda: clicked(kode_sektor.get),background="#6A6A6A", activebackground="#707070", foreground="#FFFFFF", activeforeground="#FFFFFF")
-    saveButton.grid(column=0, row=13, padx=10, pady=2, sticky=W)
-
-    quitButton = Button(master=sektor_window_frame, text="Cancel", command=sektor_window.destroy, background="#6A6A6A", activebackground="#707070", foreground="#FFFFFF", activeforeground="#FFFFFF")
-    quitButton.grid(column=0, row=13, sticky=E)
-
-
-    # MySQL Parameter
-    koneksi.commit()
-    koneksi.close()
-
-
-# method untuk memilih ticker saham setelah memilih sektor
-def pilihTickerSaham():
-    # second widget untuk memilih jenis sektor
-    tickerSaham_window = Toplevel()
-    tickerSaham_window.title('Pilih Ticker Saham')
-    tickerSaham_window.iconbitmap("D:/Aji/[ICON FILE]/business-color_stock_icon-icons.com_53431.ico")
-    tickerSaham_window.geometry("400x400")
-    
-    # MySQL Parameter
-    koneksi = Connection(host='localhost', user='root', database='saham_indonesia')
-    kursor = koneksi.cursor()
-    
-
-    saveButton = Button(master=tickerSaham_window, text="Save Option")
-    saveButton.grid(column=0, row=12, padx=10, pady=2, sticky=W)
-
-    quitButton = Button(master=tickerSaham_window, text="Cancel", command=tickerSaham_window.destroy)
-    quitButton.grid(column=0, row=12, sticky=N+S)
-    
-    # MySQL Parameter
-    koneksi.commit()
-    koneksi.close()
-
+    Unselect_Option_Saham = tkinter.Button(
+        master=Mini_Frame, text="Hapus Pilihan",
+        background="#535353", foreground="#ffffff", activebackground="#707070", activeforeground="#ffffff", 
+        command=Pilihan_Saham.deselect()
+    )
+    Unselect_Option_Saham.grid(column=1, row=0, sticky=tkinter.E)
 
 # method untuk memanggil dataframe
 def call_dataframe():
@@ -160,68 +117,74 @@ def call_dataframe():
         end=endDate.get()
     )
 
-    # menampilkan dataframe
-    df_label = Label(master=root, text=df)
-    df_label.grid()
+### MySQL Connection
+Koneksi.commit()
+Koneksi.close()
 
 
-# Label widget
-sektorSaham = Label(master=root_frame, text="Pilih Sektor Saham", background="#535353", foreground="#FFFFFF")
-sektorSaham.grid(row=1, column=0, sticky=W, padx=10, pady=5)
-sahamLabel = Label(master=root_frame, text="Pilih Kode Saham", background="#535353", foreground="#FFFFFF")
-sahamLabel.grid(row=2, column=0, sticky=W, padx=10, pady=5)
-investLabel = Label(master=root_frame, text="Jumlah Investasi", background="#535353", foreground="#FFFFFF")
-investLabel.grid(row=3, column=0, sticky=W, padx=10, pady=2)
-waktuPembelian = Label(master=root_frame, text="Tanggal Transaksi", background="#535353", foreground="#FFFFFF")
-waktuPembelian.grid(row=4, column=0, sticky=W, padx=10, pady=5)
+### GUI Apps
+## Top Level Widget
+Main_Window = tkinter.Tk()
+Main_Window.title('Optimasi Portofolio Saham')
+Main_Window.iconbitmap('D:/LATIHAN PEMROGRAMAN/(METODE PENELITIAN)/icon/business_stock.ico')
+Main_Frame = tkinter.Frame(master=Main_Window, background="#535353", height=300, width=400)
+Main_Frame.pack(fill=BOTH, anchor=CENTER, side=TOP)
 
-# Button widget
-sektor_select = Button(master=root_frame, text="Daftar Sektor", command=pilihSektor, background="#6A6A6A", activebackground="#707070", foreground="#FFFFFF", activeforeground="#FFFFFF")
-sektor_select.grid(row=1, column=1, sticky=W)
-ticker_select = Button(master=root_frame, text="Ticker Saham", command=pilihTickerSaham, background="#6A6A6A", activebackground="#707070", foreground="#FFFFFF", activeforeground="#FFFFFF")
-ticker_select.grid(row=2, column=1, sticky=W)
-kalkulasi_button = Button(master=root_frame, text="Calculate Portfolio's", background="#6A6A6A", activebackground="#707070", foreground="#FFFFFF", activeforeground="#FFFFFF")
-kalkulasi_button.grid(row=5, column=1, sticky=W)
-exit_button = Button(master=root_frame, text="Quit Apps", command=quit, anchor=CENTER, background="#6A6A6A", activebackground="#707070", foreground="#FFFFFF", activeforeground="#FFFFFF")
-exit_button.grid(row=5, column=1, sticky=E)
+## Label Widget
+Label_Sektor = tkinter.Label(master=Main_Frame, background="#535353", foreground="#ffffff", justify=LEFT, text="Sektor Emiten")
+Label_Saham = tkinter.Label(master=Main_Frame, background="#535353", foreground="#ffffff", justify=LEFT, text="Daftar Saham")
+Label_Investasi = tkinter.Label(master=Main_Frame, background="#535353", foreground="#ffffff", justify=LEFT, text="Jumlah Investasi")
+Label_Tanggal = tkinter.Label(master=Main_Frame, background="#535353", foreground="#ffffff", justify=LEFT, text="Tanggal Transaksi")
+Label_Sektor.grid(column=0, row=0, padx=10, pady=5, sticky=tkinter.W)
+Label_Saham.grid(column=0, row=1, padx=10, pady=5, sticky=tkinter.W)
+Label_Investasi.grid(column=0, row=2, padx=10, pady=5, sticky=tkinter.W)
+Label_Tanggal.grid(column=0, row=3, padx=10, pady=5, sticky=tkinter.W)
 
-# Input kedalam program
-investEntry = Entry(
-    master=root_frame,
-    width=35,
-    background="#2D2D2D",
-    foreground="#FFFFFF",
-    insertbackground="#FFFFFF"
-)
-investEntry.grid(row=3, column=1)
+## Input widget
+Input_Sektor = ttk.Combobox(master=Main_Frame, width=37, height=1, justify=LEFT, values=Nama_Sektor, textvariable=Kode_Sektor)
+Input_Sektor.grid(column=1, row=0, padx=10, pady=5, sticky=tkinter.W)
 
-startDate = Entry(
-    master=root_frame,
-    width=15,
-    background="#2D2D2D",
-    foreground="#FFFFFF",
-    insertbackground="#FFFFFF"
-)
+Pilih_Saham = tkinter.Button(
+    master=Main_Frame, 
+    text="Pilih Saham", 
+    background="#535353", 
+    foreground="#ffffff", 
+    activebackground="#707070", 
+    activeforeground="#ffffff",
+    command=Select_Stocks
+    )
+Pilih_Saham.grid(column=1, row=1, padx=10, pady=5, sticky=tkinter.W)
+
+Input_Modal_Investasi = tkinter.Entry(master=Main_Frame, width=40, background="#2D2D2D", foreground="#FFFFFF", insertbackground="#FFFFFF")
+Input_Modal_Investasi.grid(column=1, row=2, padx=10, pady=5, sticky=tkinter.W)
+
+# Tanggal transaksi
+Mini_Frame = tkinter.Frame(master=Main_Frame, width=40, height=1, background="#535353") # Mini frame untuk mengatur layout 3 widget dalam satu kolom
+Mini_Frame.grid(column=1, row=3, padx=10, pady=5, sticky=tkinter.W)
+
+startDate = tkinter.Entry(master=Mini_Frame, width=15, background="#2D2D2D", foreground="#FFFFFF", insertbackground="#FFFFFF")
 startDate.insert(0, "e.g. 2020/12/31")
-startDate.grid(row=4, column=1, sticky=W)
+startDate.grid(column=0, row=0, sticky=tkinter.W)
 
-sampaiDengan = Label(master=root_frame, text="s/d", background="#535353", foreground="#FFFFFF")
-sampaiDengan.grid(row=4, column=1)
+sampaiDengan = tkinter.Label(master=Mini_Frame, text="s/d", background="#535353", foreground="#FFFFFF", width=7)
+sampaiDengan.grid(column=1, row=0, sticky=tkinter.W)
 
-endDate = Entry(
-    master=root_frame,
-    width=15,
-    background="#2D2D2D",
-    foreground="#FFFFFF",
-    insertbackground="#FFFFFF"
-)
+endDate = tkinter.Entry(master=Mini_Frame, width=15, background="#2D2D2D", foreground="#FFFFFF", insertbackground="#FFFFFF")
 endDate.insert(0, "e.g. 2021/12/31")
-endDate.grid(row=4, column=1, sticky=E)
+endDate.grid(column=2, row=0, sticky=tkinter.W)
+
+## Process Widget
+Button_Frame = tkinter.Frame(master=Main_Frame, width=40, height=1, background="#535353")
+Button_Frame.grid(column=1, row=4, padx=10, pady=5, sticky=tkinter.W)
+
+Optimum_Button = tkinter.Button(master=Button_Frame, text="Optimasi Saham", background="#535353", foreground="#FFFFFF", activebackground="#707070", activeforeground="#ffffff")
+Optimum_Button.grid(column=0, row=0, sticky=tkinter.W)
+
+Exit_Button = tkinter.Button(master=Button_Frame, text="Keluar Aplikasi", background="#535353", foreground="#FFFFFF", activebackground="#707070", activeforeground="#ffffff", width=12, command=quit)
+Exit_Button.grid(column=1, row=0, sticky=tkinter.E, padx=50)
 
 
 
-# MySQL Parameter
-koneksi.commit()
-koneksi.close()
-# tkinter Parameter
-root.mainloop()
+
+### GUI Apps
+Main_Window.mainloop()
